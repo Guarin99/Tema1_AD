c1dac64ddbed5f69fdc7c3eed279e5d7fc95e46e
Guarin99 <ruben.serrano262@comunidadunir.net>
2025-09-27 10:11:12 +0200

    25/09/2025
    Inputs desde ficheros

---
+++ FicherosInformacion/src/Entrada.java
@@ -0,0 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Entrada {
+
+    public static void main(String[] args) {
+        Operaciones operaciones = new Operaciones();
+       // operaciones.informacionFichero("src/resources/ejemplo1.txt");
+        operaciones.informacionDirectorio("src/resources");
+    }
+
+}
